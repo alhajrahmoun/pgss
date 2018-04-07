d2aaf36e745c68bf21eb28cea8d453beb8766c86
mhdrahmoun <mrahmoun93@gmail.com>
2018-04-07 18:37:13 +0300

    refactoring filters

--- app/controllers/api/filters_controller.rb
+++ app/controllers/api/filters_controller.rb
@@ -1,6 +1,6 @@
 class Api::FiltersController < ApplicationController
     def index
         # @filters = Filter.
-        render json: {filters: Filter.all}
+        render json: {filters: Filter.select(:id, :name, :filter_type).group_by(&:filter_type)}
     end
 end
